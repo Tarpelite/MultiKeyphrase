2276e2c0315d0fe19921979c8b280d3375265145
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-20 13:59:21 +0800

    fix bugs

--- src/biunilm/evaluate_1pair.py
+++ src/biunilm/evaluate_1pair.py
@@ -399,8 +399,8 @@ def main():
 
                     text_tk = tokenizer.tokenize(text)
                     j = 0
-                    while j < len(sent_collect) and len(text_tk) + len(tokenizer.tokenize(sents_collect[j])) <= args.max_seq_length:
-                        text += " " + sent_collect[j]
+                    while j < len(sents_collect) and len(text_tk) + len(tokenizer.tokenize(sents_collect[j])) <= args.max_seq_length:
+                        text += " " + sents_collect[j]
                         j += 1
                     
                     input_lines.append(text)                
